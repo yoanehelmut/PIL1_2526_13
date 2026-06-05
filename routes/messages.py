4128e8bba077f3b0e6f6cb00ca88728d193ec071
Rinaldi Da Silveira <rinaldidasilveira@gmail.com>
2026-06-05 12:38:22 +0100

    fix: correction fin de code messages.py

--- routes/messages.py
+++ routes/messages.py
@@ -37,7 +37,7 @@ def get_messages(conv_id):
     conn.close()
     return jsonify(msgs)
 
-@messages_bp.route("/conversation/nouvelle", methods=["POST"]
+@messages_bp.route("/conversation/nouvelle", methods=["POST"])
 def nouvelle_conversation():
     if "user_id" not in session:
         return jsonify({"error": "Non connecte"}), 401
