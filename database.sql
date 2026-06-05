519447af5a8dce29af7b8a6be83dd0bb58f18803
raphaeltchiakpe-blip <raphdave119@gmail.com>
2026-06-05 13:37:46 +0100

    Apport d'amelioration à la base de données

--- database.sql
+++ database.sql
@@ -4,14 +4,18 @@ nom VARCHAR(100) NOT NULL,
 prenom VARCHAR(100) NOT NULL,
 email VARCHAR (150) UNIQUE NOT NULL,
 mot_de_passe_hash TEXT NOT NULL,
+role ENUM('Mentor','Etudiant')NOT NULL,
 filiere VARCHAR(100),
 niveau VARCHAR(50),
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+reset_token VARCHAR(255)NULL,
+reset_token_expires TIMESTAMP NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE competences (
 id INT AUTO_INCREMENT PRIMARY KEY,
-nom VARCHAR(100) NOT NULL
+nom VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE user_competences (
@@ -44,7 +48,7 @@ ON DELETE CASCADE
 CREATE TABLE offres_demandes (
 id INT AUTO_INCREMENT PRIMARY KEY,
 user_id INT NOT NULL,
-type VARCHAR(10)NOT NULL,
+type VARCHAR('offre','demande')NOT NULL,
 description TEXT,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
@@ -55,14 +59,19 @@ ON DELETE CASCADE
 
 CREATE TABLE matchings (
 id INT AUTO_INCREMENT PRIMARY KEY,
-user1_id INT NOT NULL,
-user2_id INT NOT NULL,
-score DECIMAL(5,2),
+etudiant_id INT NOT NULL,
+mentor_id INT NOT NULL,
+score DECIMAL(5,2) NOT NULL,
+statut ENUM('en_attente','accepte','refuse') DEFAULT 'en_attente',
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-FOREIGN KEY (user1_id)
+updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+FOREIGN KEY (etudiant_id)
 REFERENCES users(id)
 ON DELETE CASCADE
+FOREIGN KEY (mentor_id)
+REFERENCES users(id)
+ON DELETE CASCADE
+
 );
 
 CREATE TABLE messages (
@@ -70,6 +79,7 @@ id INT AUTO_INCREMENT PRIMARY KEY,
 expediteur_id INT NOT NULL,
 destinataire_id INT NOT NULL,
 contenu TEXT NOT NULL,
+is_read BOOLEAN DEFAULT FALSE,
 timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
 FOREIGN KEY (expediteur_id)
@@ -80,3 +90,7 @@ FOREIGN KEY (destinataire_id)
 REFERENCES users(id)
 ON DELETE CASCADE
 );
+CREATE INDEX idx_users_email ON users(email);
+CREATE INDEX idx_users_role ON users(role);
+CREATE INDEX idx_matching_score ON matching(score);
+
